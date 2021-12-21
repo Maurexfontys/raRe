89b58989a47fdec3b8be3721c655326af8ebf5f5
Maurice <m.waelen@student.fontys.nl>
2021-12-21 20:00:19 +0100

    node version 16.13.1

--- src/components/raRe/pages/Landing.js
+++ src/components/raRe/pages/Landing.js
@@ -3,9 +3,9 @@ import React from "react";
 class Landing extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Rare</h1>
-      </div>
+      <>
+        <h1>test</h1>
+      </>
     );
   }
 }
